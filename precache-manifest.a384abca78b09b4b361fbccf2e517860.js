e1c29d0c6c7508eda80dd20babf7a4405bfb0ad5
Sasha Russu <russusasha19@gmail.com>
2019-11-01 10:41:52 +0200

    Updates

---
+++ precache-manifest.a384abca78b09b4b361fbccf2e517860.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4835917da019d600e750187c2a4cfa0",
+    "url": "/passwords-manager/index.html"
+  },
+  {
+    "revision": "26be4135f862c081e809",
+    "url": "/passwords-manager/static/css/main.a576c54a.chunk.css"
+  },
+  {
+    "revision": "8a726f9949c3eec012ba",
+    "url": "/passwords-manager/static/js/2.fc579257.chunk.js"
+  },
+  {
+    "revision": "26be4135f862c081e809",
+    "url": "/passwords-manager/static/js/main.79d14c5f.chunk.js"
+  },
+  {
+    "revision": "6e5a7569a3bad0557066",
+    "url": "/passwords-manager/static/js/runtime-main.a79d8d88.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c4e710f950e57e6dd4d227010f363d1.js"
+  "/passwords-manager/precache-manifest.a384abca78b09b4b361fbccf2e517860.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/passwords-manager/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
